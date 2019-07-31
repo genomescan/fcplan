480ddf6eda63c8cfcfcd3ee306b877183d1e18d1
Niels de Water <n.dewater@genomescan.nl>
2019-07-31 18:25:18 +0200

    u

--- sv/settings.py
+++ sv/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'm48#u)7w1-eygicmbx+9z0v(c%%=trh-3p3!50cd$pg&@ibcui'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','lims']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','lims', 'nielswork']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
 MEDIA_URL = '/media/'
